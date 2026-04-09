36880e4ef4a821fd57ea9f801f8117ae80dde596
Josh Oertel <16182568+joshwashywash@users.noreply.github.com>
2026-04-09 18:21:26 -0500

    export db files from server app

--- apps/client/src/routes/tasks/+page.server.ts
+++ apps/client/src/routes/tasks/+page.server.ts
@@ -1,15 +1,16 @@
 import { error } from '@sveltejs/kit';
 
 import { API_URL } from '$env/static/private';
+import type { Task } from '@mrmittens/server/schema';
 
 export async function load({ fetch, params }) {
 	const res = await fetch(`${API_URL}/tasks`);
 
 	if (!res.ok) throw error(500, 'Failed to fetch posts');
 
-	const tasks = await res.json();
+	const tasks: Task[] = await res.json();
 	// TODO generate drizzle types for shared usage
-	const tasksByUser = Object.groupBy(tasks, (task: any) => task.user.displayName);
+	const tasksByUser = Object.groupBy(tasks, (task) => task.user.displayName);
 
 	return { tasksByUser };
 }
